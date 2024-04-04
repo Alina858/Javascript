b040eb70de0ef9b4c4406de891dbc1f1f5d3fa6a
Alina Omar <alinaomar13@gmail.com>
2024-04-04 12:51:51 +0100

    i added conditionals

---
+++ conditional.js
@@ -0,0 +1,36 @@
+
+
+// if if else if switch case default
+
+
+let age = 2;
+
+if(age>=18 && age <= 60) {
+
+console.log("you can watch the movie in Cinema,Please pay") }
+else if (age > 60) {
+
+    console.log("You can watch the movie in Cinema, dont pay")
+}
+
+ else{
+console/log("You cannot watch the movie in Cinema, you are a minor")
+
+}
+
+switch (age) {
+    case 18:
+        console.log("you can watch the movie in Cinema,Please pay")
+     break;
+     case 60:
+         console.log("You can watch the movie in Cinema, dont pay")
+         break;
+    default:
+        console/log("You cannot watch the movie in Cinema, you are a minor")
+        break;
+}
+
+
+
+
+
